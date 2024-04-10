ca9e7959d619d2eddd99c92f3232a32d2387002c
Benjamin Dam Christensen <bdch54128@edu.ucl.dk>
2024-04-10 12:04:09 +0200

    tilføjet item service og lavet mappe

---
+++ DogApp.Services/Interfaces/IItemService.cs
@@ -0,0 +1,12 @@
+﻿using DogApp.Data.EntityModels;
+
+namespace DogApp.Services.InterfaceService;
+
+public interface IItemService
+{
+    Task CreateItem(Item item);
+
+    Task<List<Item>> GetAllItems();
+    Task<Item> GetItemById(int id);
+    Task UpdateItemById(Item item);
+}
---
+++ DogApp.Services/ItemService.cs
@@ -0,0 +1,40 @@
+﻿using DogApp.Data.EntityModels;
+using DogApp.Repository;
+using DogApp.Services.InterfaceService;
+
+
+namespace DogApp.Services;
+
+public class ItemService(IItemRepo itemRepo) : IItemService
+{
+    private readonly IItemRepo _itemrepo = itemRepo ?? throw new ArgumentNullException(nameof(itemRepo));
+    public async Task CreateItem(Item item)
+    {
+        ArgumentNullException.ThrowIfNull(item);
+        await _itemrepo.AddAsync(item);
+    }
+
+    public async Task<List<Item>> GetAllItems()
+    {
+        return await _itemrepo.GetAllAsync();
+    }
+    public async Task<Item?> GetItemById(int id)
+    {
+        ArgumentNullException.ThrowIfNull(id);
+        return await _itemrepo.GetByIdAsync(id);
+    }
+
+    public async Task UpdateItemById(Item item)
+    {
+        var itemToUpdate = await _itemrepo.GetByIdAsync(item.Id);
+
+        if (itemToUpdate != null)
+        {
+            await _itemrepo.UpdateAsync(itemToUpdate);
+        }
+        else
+        {
+            ArgumentNullException.ThrowIfNull(item);
+        }
+    }
+}
