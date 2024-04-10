3411b9b19bad4255a6ac1ae19beb67fd5f091944
Benjamin Dam Christensen <bdch54128@edu.ucl.dk>
2024-04-10 12:05:20 +0200

    added constructors to itemdto

--- DogApp.API/Dto/ItemDto.cs
+++ DogApp.API/Dto/ItemDto.cs
@@ -3,12 +3,24 @@
     public class ItemDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public string? Description { get; set; }
         public string? Image { get; set; }
         public enum Category { Sign, Extra }
         public Category ItemCategory { get; set; }
         public ICollection<TrackItem> TrackItems { get; set; }
+
+        public ItemDto(int id, string? name, string? description, string? image)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Image = image;
+        }
+        public ItemDto()
+        {
+
+        }
 
     }
 }
