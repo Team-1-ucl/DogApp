2960ca6de90b5e4ad71bc92926c15c234dee7a0b
benjamin <Benjamintrue0549@gmail.com>
2024-04-22 10:02:26 +0200

    rettet item pages

--- DogApp.Web/Dto/ItemDtos/ItemDto.cs
+++ DogApp.Web/Dto/ItemDtos/ItemDto.cs
@@ -8,7 +8,7 @@ public class ItemDto
     public string? Name { get; set; }
     public string? Description { get; set; }
     public string? Image { get; set; }
-    public bool? isSign { get; set; }
+    public bool? IsSign { get; set; }
     public string? Category { get; set; }
     public ICollection<TrackItem>? TrackItems { get; set; }
 
